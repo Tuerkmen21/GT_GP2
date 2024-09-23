3ac045bd7000ec1c23fd30fb523cc1ad344f3686
Adnan_Tuerkmen <tuerkmen.a21@gmail.com>
2024-09-23 12:23:41 +0200

    test

---
+++ test.py
@@ -0,0 +1,22 @@
+import streamlit as st
+import pandas as pd
+
+st.write("test")
+st.header ("my header")
+st.subheader("header")
+st.write("test21")
+
+thisdict = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+
+mylist = ["name1,name2,name3"]
+st.write(thisdict)
+
+
+st.write("my text")
+
+st.write(mylist)
+
